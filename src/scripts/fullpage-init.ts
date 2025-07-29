1a09198ab174000e7d51af9fe613041a0b863051
Catasi <aideecasillas14@gmail.com>
2025-07-29 17:52:02 -0600

    configuraciones fullpage

--- src/scripts/fullpage-init.ts
+++ src/scripts/fullpage-init.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2025 Aideé Casillas
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
 // src/scripts/fullpage-init.ts
 import $ from 'jquery';
 
@@ -10,11 +20,18 @@ declare global {
 $(function () {
   new window.fullpage('#fullpage', {
     sectionsColor: [],
-    anchors: ['firstPage', 'secondPage'],
+    anchors: [],
     menu: '#menu',
     slidesNavigation: true,
     navigation: true,
+    navigationPosition: 'left',
     scrollOverflow: true,
     scrollOverflowReset: true,
+    autoScrolling: true,
+    responsiveSlides: true,
+    TouchSensitivity: 10,
+    continuousVertical: true,
+    continuousHorizontal: true,
+    fixedElements: '#header, #footer',
   });
 });
